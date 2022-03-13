01d6ed5000f830fdd91c0664e908c0ff338ab9ad
angel <fsguoangel@hotmail.com>
2022-03-13 23:11:32 +0800

    graphql mutation inside

--- resources/js/components/CardAddButton.vue
+++ resources/js/components/CardAddButton.vue
@@ -1,3 +1,16 @@
 <template>
-    <div class="rounded-sm p-2 text-gray-600 cursor-pointer hover:bg-gray-400 hover:text-gray-800 text-sm">Add a New Card</div>
+    <div @click="addCard" class="rounded-sm p-2 text-gray-600 cursor-pointer hover:bg-gray-400 hover:text-gray-800 text-sm">Add a New Card</div>
 </template>
+<script>
+import CardAdd from "./../graphql/CardAdd.gql";
+export default {
+    methods: {
+        addCard() {
+            this.$apollo.mutate({
+                mutation: CardAdd,
+                variables: { title: "aaafff", listId: 1, order: 1 }
+            });
+        }
+    }
+}
+</script>
