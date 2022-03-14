deddbdc13949a8efe31fbf6dd59e6f3c19be6706
angel <fsguoangel@hotmail.com>
2022-03-14 11:35:04 +0800

    security - authentication

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,13 @@ import Vue from 'vue';
 import apolloProvider from './apollo.config';
 import Board from './Board';
 import './bootstrap';
-
+import router from "./router.config";
 window.Vue = Vue;
 
 Vue.component('Board', Board);
 
 const app = new Vue({
     el: '#app',
-    apolloProvider
+    apolloProvider,
+    router
 });
